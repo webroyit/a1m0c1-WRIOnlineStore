c7842eb03bdbfca91bb5a6dde24e50cff2c59917
royweb <xytank@gmail.com>
2021-01-02 12:14:06 -0500

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+    return(
+        <div className='header'>
+            <img className="header__logo" src="/images/logo.png" alt="Logo" />
+
+            <div className="header__search">
+                <input
+                    className="header__searchInput"
+                    type="text" />
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello Guest
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign In
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Returns
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Orders
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Your
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Membership
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
