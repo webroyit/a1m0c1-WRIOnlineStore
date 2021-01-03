ed28d8ed67d289d7d06325ba7a48b1be31459d09
royweb <xytank@gmail.com>
2021-01-03 15:25:45 -0500

    Make the product component show dynamic content

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,18 +10,48 @@ function Home() {
                 <img className="home__image" src="/images/hero.jpe" alt="Hero" />
                 
                 <div className="home__row">
-                    <Product />
-                    <Product />
+                    <Product
+                        id="1231231293"
+                        title="Basket of Flowers (12 Different kinds of flowers)"
+                        price={19.99}
+                        image="/images/product3.jpe"
+                        rating={5} />
+                    <Product
+                        id="3520932423"
+                        title="Chair (5 foots long and well made in platic)"
+                        price={15.99}
+                        image="/images/product5.jpe"
+                        rating={4} />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product
+                        id="9549550234"
+                        title="Water Bottle (14 Inches long and made of glass)"
+                        price={10.99}
+                        image="/images/product1.jpe"
+                        rating={2} />
+                    <Product
+                        id="7532949324"
+                        title="Clock (9 Inches long and made of plastic)"
+                        price={9.99}
+                        image="/images/product4.jpe"
+                        rating={3} />
+                    <Product
+                        id="5248924623"
+                        title="Pencil Case (24 Different colors of pencils)"
+                        price={6.99}
+                        image="/images/product2.jpe"
+                        rating={4} />
                 </div>
 
                 <div className="home__row">
-                    <Product />
+                    <Product
+                        id="2230249022"
+                        title="Sofa (12 Foot wide and 5 Foot heigth made with cotton)"
+                        price={59.99}
+                        image="/images/product6.jpe"
+                        rating={5} />
                 </div>
             </div>
         </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 import './Product.css';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
     return(
         <div className='product'>
             <div className="product__info">
-                <p>Water Bottle</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>10.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>&#127775;</p>
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>&#127775;</p>
+                        ))
+                    }
                 </div>
             </div>
 
-            <img src="/images/product1.jpe" alt="Product" />
+            <img src={image} alt="Product" />
 
             <button>Add to Basket</button>
         </div>
