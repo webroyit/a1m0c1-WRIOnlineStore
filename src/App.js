a512a9ca48a209e8ce280437a274ed912c6b5b79
royweb <xytank@gmail.com>
2021-01-08 13:32:57 -0500

    Use onAuthStateChanged() to check the user auth status and store their data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+import { useStateValue } from './StateProvider';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './App.css';
@@ -5,8 +7,34 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
 import Login from './components/Login';
+import { auth } from './firebase';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  
+  useEffect(() => {
+    // Will only run once when the app component loads
+
+    // An listener to check if the user if login or not
+    auth.onAuthStateChanged(authUser => {
+      console.log(authUser);
+
+      if (authUser) {
+        // The user just in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        });
+      } else {
+        // The user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        });
+      }
+    })
+  }, []);
+
   return (
     <Router>
       <div className="App">
