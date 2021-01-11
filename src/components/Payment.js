e5ff475eea0091af85ab4e8a1c89249c6696ec1d
royweb <xytank@gmail.com>
2021-01-11 12:29:13 -0500

    Display total price and add a payment button on the payment page

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
 
 import './Payment.css';
 import  { useStateValue } from '../StateProvider';
+import { getBasketTotal } from '../reducer';
 import CheckoutProduct from './CheckoutProduct';
 
 function Payment() {
@@ -11,6 +13,17 @@ function Payment() {
 
     const stripe = useStripe();
     const elements = useElements();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleChnage = event => {
+        // Listen for changes in the CardElement and display any errors as the user types their card details
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "");
+    }
 
     return(
         <div className='payment'>
@@ -52,7 +65,29 @@ function Payment() {
                     </div>
                     <div className='payment__details'>
                         <form>
-                            <CardElement />
+                            <CardElement onChange={handleChnage} />
+
+                            <div className="payment__priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <>
+                                            <h3>
+                                                Order Total: {value}
+                                            </h3>
+                                        </>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                </button>
+
+                                {error && <div>{error}</div>}
+                            </div>
                         </form>
                     </div>
                 </div>
