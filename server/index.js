07917d3851bf4aa91bf9e9daa727d51440b859a6
royweb <xytank@gmail.com>
2021-01-12 13:22:46 -0500

    Add a route to send client secret to the front end

--- server/index.js
+++ server/index.js
@@ -12,7 +12,19 @@ app.use(express.json());        // Send or pass data in json format
 
 // API routes
 app.get('/', (request, response) => response.status(200).send('WRI Online Store'));
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,      // Subunits of the currency
+        currency: "usd"
+    });
+    
+    // Ok - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret
+    })
+})
 
 const PORT = process.env.PORT || 1000;
-
 app.listen(PORT, console.log(`Server running in ${PORT}`));
